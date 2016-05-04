4bfb7055d9f57a5f18dc43cd53451401527f9402
paulit81 <haspau03@student.westerdals.no>
2016-05-04 10:01:58 +0200

    added a new button with func

--- DogYearConverter/ViewController.swift
+++ DogYearConverter/ViewController.swift
@@ -57,5 +57,64 @@ class ViewController: UIViewController {
         
     }
 
+    @IBAction func convertToRealDogYearButtonPressed(sender: UIButton) {
+        
+        let humanYearsFromTextField = Int(humanYearsConverterTextField.text! )
+        
+        if( humanYearsFromTextField == nil){
+            
+            dogYearLabel.font = UIFont.systemFontOfSize(12)
+            dogYearLabel.textColor = UIColor.redColor()
+            dogYearLabel.text = "Error you have not inserted a value, try again!"
+        }
+        
+        else if( humanYearsFromTextField < 0){
+            
+            dogYearLabel.font = UIFont.systemFontOfSize(12)
+            dogYearLabel.textColor = UIColor.redColor()
+            dogYearLabel.text = "Error you have enter a minus value, try again!"
+        }
+          // multiple with 10.5 for first two, after multiple with four
+            
+        else if( humanYearsFromTextField < 2){
+            
+            let humanYearsFromTextField = Int(humanYearsConverterTextField.text! )
+            
+            dogYearLabel.font = UIFont.italicSystemFontOfSize(15)
+            let yearConverter = 4
+            dogYearLabel.textColor = UIColor.blackColor()
+            dogYearLabel.hidden = false
+            dogYearLabel.text = "\(humanYearsFromTextField)"
+            
+            dogYearLabel.text = "\(humanYearsFromTextField! ) dog years is: " + "\(humanYearsFromTextField! * yearConverter)" + " years in human years"
+            
+            humanYearsConverterTextField.isFirstResponder()
+            humanYearsConverterTextField.text = ""
+            humanYearsConverterTextField.resignFirstResponder()
+            
+        }
+        else if( humanYearsFromTextField >= 2 &&
+            
+            humanYearsFromTextField > 0){
+            
+              let humanYearsFromTextField = Double(humanYearsConverterTextField.text! )
+            
+            dogYearLabel.font = UIFont.italicSystemFontOfSize(15)
+            let yearConverter = 10.5
+            dogYearLabel.textColor = UIColor.blackColor()
+            dogYearLabel.hidden = false
+            dogYearLabel.text = "\(humanYearsFromTextField)"
+            
+            dogYearLabel.text = "\(humanYearsFromTextField! ) dog years is: " + "\(humanYearsFromTextField! * yearConverter)" + " years in human years"
+            
+            humanYearsConverterTextField.isFirstResponder()
+            humanYearsConverterTextField.text = ""
+            humanYearsConverterTextField.resignFirstResponder()
+            
+            
+        }
+      
+        
+    }
 }
 
